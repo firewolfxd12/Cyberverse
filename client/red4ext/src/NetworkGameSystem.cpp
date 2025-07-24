f7f3e44d61f9cb16b0eeea708056755fd8401dae
Nicholas Swain Durbin <64529566+firewolfxd12@users.noreply.github.com>
2025-07-24 03:18:35 +0300

    Use ESteamNetworkingSendFlags

--- client/red4ext/src/NetworkGameSystem.cpp
+++ client/red4ext/src/NetworkGameSystem.cpp
@@ -177,7 +177,7 @@ bool NetworkGameSystem::EnqueueMessage(uint8_t channel_id, T content)
 
     assert(data.size() < std::numeric_limits<uint32_t>::max());
 
-    int flags = k_nSteamNetworkingSend_Reliable;
+    ESteamNetworkingSendFlags flags = k_nSteamNetworkingSend_Reliable;
     if (channel_id == 1)
     {
         flags = k_nSteamNetworkingSend_Unreliable;
